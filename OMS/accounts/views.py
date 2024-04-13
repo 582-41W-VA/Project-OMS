2a47d2834b58d8a2eb443418cfbfd237669d381d
tanveerali13 <tanveer.nsu13@gmail.com>
2024-04-12 22:17:28 -0400

    Apply pylint and black formatter

--- OMS/accounts/views.py
+++ OMS/accounts/views.py
@@ -1,22 +1,28 @@
-import requests
+"""
+Module Docstring:
+
+This module contains views and functions for managing user authentication, order management,
+user management, and generating reports in a web application.
+"""
+
+from datetime import datetime
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.db.models import Q, F, Count
+from django.db.models import Q
 from django.shortcuts import get_object_or_404
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import Group
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from datetime import datetime
-from .models import *
+
+from .models import Order, Comment, User
 from .forms import OrderForm, CreateUserForm, UpdateUserForm
 from .orderFilters import OrderFilter
 from .userFilters import UserFilter
 from .decorators import unauthenticated_user, allowed_users
 
 
-#REGISTER
+# REGISTER
 @unauthenticated_user
 def registerPage(request):
     """
@@ -36,18 +42,19 @@ def registerPage(request):
             - Re-renders the registration form page with error messages.
     """
     form = CreateUserForm()
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CreateUserForm(request.POST)
         if form.is_valid():
             user = form.save()
-            user.groups.add(Group.objects.get(name='worker'))
-            messages.success(request, 'Account was created for ' + user.username)
-            return redirect('login')
+            user.groups.add(Group.objects.get(name="worker"))
+            messages.success(request, "Account was created for " + user.username)
+            return redirect("login")
 
-    context = {'form': form}
-    return render(request, 'accounts/register.html', context)
+    context = {"form": form}
+    return render(request, "accounts/register.html", context)
 
-#LOGIN
+
+# LOGIN
 @unauthenticated_user
 def loginPage(request):
     """
@@ -63,22 +70,23 @@ def loginPage(request):
         - If authentication is successful, logs the user in and redirects to the home page.
         - If authentication fails, displays an error message and re-renders the login page.
     """
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
 
         user = authenticate(request, username=username, password=password)
 
         if user is not None:
             login(request, user)
-            return redirect('home')
-        else:
-            messages.info(request, 'Username OR password is incorrect')
+            return redirect("home")
+
+        messages.info(request, "Username OR password is incorrect")
 
     context = {}
-    return render(request, 'accounts/login.html', context)
+    return render(request, "accounts/login.html", context)
 
-#LOGOUT
+
+# LOGOUT
 def logoutUser(request):
     """
     View function for handling user logout.
@@ -90,12 +98,13 @@ def logoutUser(request):
     - Redirects the user to the login page after logging them out.
     """
     logout(request)
-    return redirect('login')
+    return redirect("login")
+
 
 ####DASHBOARD####
-#HOME
-@login_required(login_url='login')
-@allowed_users(allowed_roles=['admin', 'manager', 'worker'])
+# HOME
+@login_required(login_url="login")
+@allowed_users(allowed_roles=["admin", "manager", "worker"])
 def home(request):
     """
     View function for the home page/dashboard.
@@ -114,25 +123,27 @@ def home(request):
     """
     orders = Order.objects.all()
 
-    total_pending = orders.exclude(status='Complete').count()
-    urgent = orders.filter(priority='Urgent').exclude(status='Complete').count()
-    regular = orders.filter(priority='Normal').exclude(status='Complete').count()
+    total_pending = orders.exclude(status="Complete").count()
+    urgent = orders.filter(priority="Urgent").exclude(status="Complete").count()
+    regular = orders.filter(priority="Normal").exclude(status="Complete").count()
 
     myFilter = OrderFilter(request.GET, queryset=orders)
     orders = myFilter.qs
 
-    context = {'orders': orders ,
-                'total_pending': total_pending,
-                'urgent': urgent,
-                'regular': regular,
-                'myFilter': myFilter,
-                'page_title': 'Dashboard'
-                }
+    context = {
+        "orders": orders,
+        "total_pending": total_pending,
+        "urgent": urgent,
+        "regular": regular,
+        "myFilter": myFilter,
+        "page_title": "Dashboard",
+    }
 
-    return render(request, 'accounts/dashboard.html', context)
+    return render(request, "accounts/dashboard.html", context)
 
-#VIEW ORDER
-@login_required(login_url='login')
+
+# VIEW ORDER
+@login_required(login_url="login")
 def viewOrder(request, pk):
     """
     View function for displaying details of a specific order.
@@ -150,22 +161,24 @@ def viewOrder(request, pk):
     order = get_object_or_404(Order, pk=pk)
     comments = Comment.objects.filter(order=order)
 
-    base_url = request.build_absolute_uri('/').rstrip('/')   
-    path = request.get_full_path() 
-    qr_data = base_url + path  
-    qr_code_url = f'http://api.qrserver.com/v1/create-qr-code/?data={qr_data}&size=100x100'
+    base_url = request.build_absolute_uri("/").rstrip("/")
+    path = request.get_full_path()
+    qr_data = base_url + path
+    qr_code_url = (
+        f"http://api.qrserver.com/v1/create-qr-code/?data={qr_data}&size=100x100"
+    )
 
     context = {
-        'order': order,
-        'comments': comments,
-        'qr_code_url': qr_code_url,
+        "order": order,
+        "comments": comments,
+        "qr_code_url": qr_code_url,
     }
 
     return render(request, "accounts/view_order.html", context)
 
 
-#CREATE ORDER
-@login_required(login_url='login')
+# CREATE ORDER
+@login_required(login_url="login")
 def createOrder(request):
     """
     View function for creating a new order.
@@ -183,22 +196,19 @@ def createOrder(request):
         * page_title: Title for the page.
     """
     form = OrderForm()
-    if request.method == 'POST':
+    if request.method == "POST":
         form = OrderForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('/')
-    
-    context = {
-        "form": form,    
-        "page_title": "Orders"
-    }
+            return redirect("/")
+
+    context = {"form": form, "page_title": "Orders"}
 
     return render(request, "accounts/order_form.html", context)
 
 
-#SEARCH ORDER
-@login_required(login_url='login')
+# SEARCH ORDER
+@login_required(login_url="login")
 def searchOrder(request):
     """
     View function for searching orders.
@@ -221,23 +231,21 @@ def searchOrder(request):
     search_query = request.GET.get("search_query")
     if search_query:
         if search_query.isdigit():
-            orders = Order.objects.filter(Q(id=int(search_query)) | Q(title__icontains=search_query))
-        else: 
+            orders = Order.objects.filter(
+                Q(id=int(search_query)) | Q(title__icontains=search_query)
+            )
+        else:
             orders = Order.objects.filter(title__icontains=search_query)
     else:
         orders = Order.objects.all()
 
-    context = {
-        "orders": orders,
-        "search_query": search_query,
-        "page_title": "Orders"
-    }
+    context = {"orders": orders, "search_query": search_query, "page_title": "Orders"}
 
     return render(request, "accounts/dashboard.html", context)
 
 
-#ADD COMMENT
-@login_required(login_url='login')
+# ADD COMMENT
+@login_required(login_url="login")
 def addComment(request, pk):
     """
     View function for adding a comment to an order.
@@ -263,15 +271,15 @@ def addComment(request, pk):
         comment_text=request.POST["comment_text"],
         order=current_order,
         user=current_username,
-        date_created=current_date
+        date_created=current_date,
     )
     new_comment.save()
 
-    return redirect('view_order', pk=pk)
+    return redirect("view_order", pk=pk)
 
 
-#DELETE COMMENT
-@login_required(login_url='login')
+# DELETE COMMENT
+@login_required(login_url="login")
 def deleteComment(request, comment_id):
     """
     View function for deleting a comment.
@@ -294,13 +302,13 @@ def deleteComment(request, comment_id):
 
     if request.user.is_superuser or request.user == comment.user:
         comment.delete()
-        return redirect('view_order', pk=comment.order.pk)
-    else:
-        return HttpResponse("You are not authorized to delete this comment.")
+        return redirect("view_order", pk=comment.order.pk)
+
+    return HttpResponse("You are not authorized to delete this comment.")
 
 
-#UPDATE ORDER
-@login_required(login_url='login')
+# UPDATE ORDER
+@login_required(login_url="login")
 def updateOrder(request, pk):
     """
     View function for updating an existing order.
@@ -320,17 +328,17 @@ def updateOrder(request, pk):
     """
     order = get_object_or_404(Order, pk=pk)
     form = OrderForm(instance=order)
-    if request.method == 'POST':
+    if request.method == "POST":
         form = OrderForm(request.POST, request.FILES, instance=order)
         if form.is_valid():
             form.save()
-            return redirect('/')
-    context = {'form': form, 'page_title': 'Orders'}
+            return redirect("/")
+    context = {"form": form, "page_title": "Orders"}
     return render(request, "accounts/update_order_form.html", context)
 
 
-#DELETE ORDER
-@login_required(login_url='login')
+# DELETE ORDER
+@login_required(login_url="login")
 def deleteOrder(request, pk):
     """
     View function for deleting an existing order.
@@ -349,18 +357,17 @@ def deleteOrder(request, pk):
     order = get_object_or_404(Order, pk=pk)
     if request.method == "POST":
         order.delete()
-        return redirect('/')
+        return redirect("/")
 
     context = {
-        'order': order,
+        "order": order,
     }
-    return render(request, 'accounts/delete_order.html', context)
-
+    return render(request, "accounts/delete_order.html", context)
 
 
 ####USERS#####
-#USER LIST
-@login_required(login_url='login')
+# USER LIST
+@login_required(login_url="login")
 def userListPage(request):
     """
     View function for displaying a list of users.
@@ -375,20 +382,18 @@ def userListPage(request):
     Returns:
     - Rendered HTML template displaying the list of users and filter form.
     """
-    admin_group = Group.objects.get(name='admin')
-
-    users = User.objects.order_by('-date_joined')
+    users = User.objects.order_by("-date_joined")
 
     myFilter = UserFilter(request.GET, queryset=users)
     users = myFilter.qs
 
-    context = {"users": users, "myFilter": myFilter, "page_title": 'Users'}
+    context = {"users": users, "myFilter": myFilter, "page_title": "Users"}
 
-    return render(request, 'accounts/users.html', context)
+    return render(request, "accounts/users.html", context)
 
 
 # VIEW USER
-@login_required(login_url='login')
+@login_required(login_url="login")
 def viewUser(request, pk):
     """
     View function for displaying details of a user.
@@ -406,20 +411,22 @@ def viewUser(request, pk):
     """
     user = get_object_or_404(User, pk=pk)
     groups = user.groups.all()
-    context = {'user': user, 'groups': groups, 'page_title': 'Users'}
+    context = {"user": user, "groups": groups, "page_title": "Users"}
 
-    return render(request, 'accounts/view_user.html', context)
+    return render(request, "accounts/view_user.html", context)
 
 
-#CREATE USER
-@login_required(login_url='login')
+# CREATE USER
+@login_required(login_url="login")
 def createUser(request):
     """
     View function for creating a new user.
 
     Renders the user creation form initially and processes the form submission.
-    If the form is submitted via POST and is valid, saves the new user and redirects to the user list page.
-    If the form is not valid or the request method is GET, renders the 'user_form.html' template with the user creation form.
+    If the form is submitted via POST and is valid,
+    saves the new user and redirects to the user list page.
+    If the form is not valid or the request method is GET,
+    renders the 'user_form.html' template with the user creation form.
 
     Parameters:
     - request: The HTTP request object.
@@ -428,92 +435,99 @@ def createUser(request):
     - Rendered HTML template displaying the user creation form or redirecting to the user list page.
     """
     form = CreateUserForm()
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/users')
-    
+            return redirect("/users")
+
     context = {"form": form, "page_title": "Users"}
 
-    return render(request, 'accounts/user_form.html', context)
+    return render(request, "accounts/user_form.html", context)
 
 
-#UPDATE USER
-@login_required(login_url='login')
+# UPDATE USER
+@login_required(login_url="login")
 def updateUser(request, pk):
     """
     View function for updating an existing user.
 
     Retrieves the user object with the specified primary key (pk) from the database.
-    Renders the user update form initially with the current user data and processes the form submission.
-    If the form is submitted via POST and is valid, updates the user's information and redirects to the user details page.
-    If the form is not valid or the request method is GET, renders the 'update_user_form.html' template with the user update form pre-filled with the current user data.
+    Renders the user update form initially with the current user data and processes
+    the form submission.
+    If the form is submitted via POST and is valid, updates the user's information
+    and redirects to the user details page.
+    If the form is not valid or the request method is GET, renders the
+    'update_user_form.html' template with the user update form pre-filled with the
+    current user data.
 
     Parameters:
     - request: The HTTP request object.
     - pk: The primary key of the user to be updated.
 
     Returns:
-    - Rendered HTML template displaying the user update form or redirecting to the user details page.
+    - Rendered HTML template displaying the user update form or redirecting to the
+      user details page.
     """
     user = get_object_or_404(User, pk=pk)
     form = UpdateUserForm(instance=user)
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UpdateUserForm(request.POST, instance=user)
         if form.is_valid():
-            selected_roles = form.cleaned_data.get('groups')
+            selected_roles = form.cleaned_data.get("groups")
             form.save()
-            return redirect('view_user', pk=pk)
+            return redirect("view_user", pk=pk)
 
-    context = {'form': form,
-               'user': user, 
-               'page_title': 'Users'
-               }
-    return render(request, 'accounts/update_user_form.html', context)
+    context = {"form": form, "user": user, "page_title": "Users"}
+    return render(request, "accounts/update_user_form.html", context)
 
 
-#DELETE USER
-@login_required(login_url='login')
+# DELETE USER
+@login_required(login_url="login")
 def deleteUser(request, pk):
     """
     View function for deleting a user.
 
     Retrieves the user object with the specified primary key (pk) from the database.
-    Checks if the user is a superuser, and if so, displays an error message and redirects to the users list page.
-    If the request method is POST, deletes the user from the database and redirects to the users list page.
-    If the request method is GET or the user is not a superuser, renders the 'delete_user.html' template with the user details.
+    Checks if the user is a superuser, and if so, displays an error message and
+    redirects to the users list page.
+    If the request method is POST, deletes the user from the database and redirects
+    to the users list page.
+    If the request method is GET or the user is not a superuser, renders the
+    'delete_user.html' template with the user details.
 
     Parameters:
     - request: The HTTP request object.
     - pk: The primary key of the user to be deleted.
 
     Returns:
-    - Rendered HTML template displaying the delete user confirmation page or redirecting to the users list page.
+    - Rendered HTML template displaying the delete user confirmation page or
+      redirecting to the users list page.
     """
     user = get_object_or_404(User, pk=pk)
 
     if user.is_superuser:
         messages.error(request, "Cannot delete superuser.")
-        return redirect('users')
+        return redirect("users")
 
     if request.method == "POST":
         user.delete()
-        return redirect('users')
+        return redirect("users")
 
-    context = {'user': user, 'page_title': 'Users'}
-    return render(request, 'accounts/delete_user.html', context)
+    context = {"user": user, "page_title": "Users"}
+    return render(request, "accounts/delete_user.html", context)
 
 
-#REPORTS
-@login_required(login_url='login')
+# REPORTS
+@login_required(login_url="login")
 def reports(request):
     """
     View function for generating reports on orders and users.
 
     Retrieves all orders and users from the database.
-    Calculates various statistics such as the total number of orders created, pending orders, urgent orders, normal orders,
+    Calculates various statistics such as the total number of orders created,
+    pending orders, urgent orders, normal orders,
     orders requiring attention, completed orders, and total number of users created.
     Renders the 'reports.html' template with the calculated statistics.
 
@@ -526,11 +540,11 @@ def reports(request):
     orders = Order.objects.all()
 
     orders_created = orders.count()
-    orders_pending = orders.exclude(status='Complete').count()
-    orders_urgent = orders.filter(priority='Urgent').exclude(status='Complete').count()
-    orders_normal = orders.filter(priority='Normal').exclude(status='Complete').count()
-    orders_attention_required = orders.filter(status='Attention Required').count()
-    orders_completed = orders.filter(status='Complete').count()
+    orders_pending = orders.exclude(status="Complete").count()
+    orders_urgent = orders.filter(priority="Urgent").exclude(status="Complete").count()
+    orders_normal = orders.filter(priority="Normal").exclude(status="Complete").count()
+    orders_attention_required = orders.filter(status="Attention Required").count()
+    orders_completed = orders.filter(status="Complete").count()
 
     users = User.objects.all()
     users_created = users.count()
@@ -543,8 +557,7 @@ def reports(request):
         "orders_attention_required": orders_attention_required,
         "orders_completed": orders_completed,
         "users_created": users_created,
-        "page_title": 'Reports',
+        "page_title": "Reports",
     }
 
-    return render(request, 'accounts/reports.html', context)
-
+    return render(request, "accounts/reports.html", context)
