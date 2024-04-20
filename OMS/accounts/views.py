001755aa75f80e6cc54641c70791547658845ac8
tanveerali13 <tanveer.nsu13@gmail.com>
2024-04-19 21:48:19 -0400

    Short list order_assigned_to list only for workers

--- OMS/accounts/views.py
+++ OMS/accounts/views.py
@@ -328,14 +328,22 @@ def updateOrder(request, pk):
     - Redirects to the home page after successful form submission.
     """
     order = get_object_or_404(Order, pk=pk)
-    form = OrderForm(instance=order)
+
+    # Filter workers group
+    workers_group = Group.objects.filter(name="worker").first()
+    workers = workers_group.user_set.all()
+
+    # Dynamically modify the form
     if request.method == "POST":
         form = OrderForm(request.POST, request.FILES, instance=order)
         if form.is_valid():
             form.save()
             return redirect("/")
-    context = {"form": form, "order": order, "page_title": "Update Order"}
+    else:
+        form = OrderForm(instance=order)
+        form.fields["order_assigned_to"].queryset = workers
 
+    context = {"form": form, "order": order, "page_title": "Update Order"}
     return render(request, "accounts/update_order_form.html", context)
 
 
